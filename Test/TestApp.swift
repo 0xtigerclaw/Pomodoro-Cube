8c53edeba214ce6f177f6c2728ffa54278ee285c
Swayam <info@thesorachain.com>
2025-09-16 16:39:14 +0200

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Swayam Shah on 16/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
